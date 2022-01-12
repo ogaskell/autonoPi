8f889eb10260fdf2dd66cd5df2293d876e8a7d7f
20GASKELLOliverA <20GASKELLOliverA@asfc.ac.uk>
2022-01-12 10:04:36 +0000

    Setup module file structure.
    I have created 3 files and a package - CV, Management and Navigation
    will store all classes and methods for their appropriate systems, and
    the hardware package will include all hardware interfaces.

---
+++ autonopi/hardware/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""Hardware interfaces package.
+
+Since hardware interfaces will use many subclasses, it will be easier to have multiple files, as such I have moved it
+into its own package instead of a single file within autonopi.
+"""
