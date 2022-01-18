d6e568973a48501822533ce7de421b4b4044539b
Oliver Gaskell <olivergaskell700@gmail.com>
2022-01-18 10:01:54 +0000

    Create basic init function for Node().
    The current init function simply stores a reference to the Node's 
    parent, as well as generating an ID if necessary. However the 
    generate_id function is just a template for now, so current all 
    generated IDs will be 0.

--- autonopi/navigation.py
+++ autonopi/navigation.py
@@ -13,5 +13,18 @@ class Navigation:
 class Node:
     """A Single node within a navigation graph."""
 
-    def __init__(self):
-        pass
+    def __init__(self, nav: Navigation, id: int = None):
+        self.parent = nav  # Store reference to parent class
+
+        if id is None:  # Check if an ID is provided.
+            self.id = self.generate_id()  # If not, generate one.
+        else:
+            self.id = id
+
+    def generate_id(self) -> int:
+        """Generate a unique ID for the current node.
+
+        Currently just a template, will need to be replaced with functional
+        code at a later date.
+        """
+        return 0
