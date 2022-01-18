0ac223731b31f346d1608bc159dec0431c7f42ea
Oliver Gaskell <olivergaskell700@gmail.com>
2022-01-18 10:03:03 +0000

    Add basic hash magic mrthod to Node.
    This currently just returns the ID of the Node. As I mention in the 
    docstring, this is technically the wrong way to do a hash function but 
    it should suffice for my use.

--- autonopi/navigation.py
+++ autonopi/navigation.py
@@ -21,6 +21,16 @@ class Node:
         else:
             self.id = id
 
+    def __hash__(self) -> int:
+        """Return the ID of the Node as a hash value.
+
+        Technically this is a very incorrect was of calculating a hash, since
+        it should be calculated based on the objects data. However, the Node
+        doesn't really have any data the hash may be calculated from, and the ID
+        will be unique so should suffice for my use.
+        """
+        return self.id
+
     def generate_id(self) -> int:
         """Generate a unique ID for the current node.
 
