d253e0519a2af8ca9061a7fda93be59269b07b4c
Oliver Gaskell <olivergaskell700@gmail.com>
2022-01-18 09:41:18 +0000

    Create blank code structure for navigation system
    The Navigation class will store the graph of nodes, as well as any
    methods used to operate on the graph, calculate paths etc.
    The Node class will be used within the NetworkX graph to represent each
    node, and will store data such as Node ID.

--- autonopi/navigation.py
+++ autonopi/navigation.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
 """Navigation module."""
+
+
+class Navigation:
+    """A class to contain the navigation system."""
+
+    def __init__(self):
+        pass
+
+
+class Node:
+    """A Single node within a navigation graph."""
+
+    def __init__(self):
+        pass
