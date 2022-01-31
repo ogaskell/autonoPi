926b46b28a787b5bc297dd0a280766b1ec172246
Oliver Gaskell <olivergaskell700@gmail.com>
2022-01-31 19:45:33 +0000

    Create Navigation.add_node().
    This function will add the node's ID to the nodelist as well as adding 
    this ID to the graph structure.

--- autonopi/navigation.py
+++ autonopi/navigation.py
@@ -11,9 +11,14 @@ class Navigation:
     def __init__(self):
         self.graph = nx.Graph()
 
+        # This list will store a list of the node's IDs. This means the graph will only have to store an integer which
+        # refers to an index in this list.
+        self.nodes = []
+
     def add_node(self, node: object) -> None:
         """Add a node to the navigation graph."""
-        pass
+        self.nodes.append(node)
+        self.graph.add_node(len(self.nodes) - 1)
 
 
 class Node:
