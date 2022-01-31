9fcce526d7d1143a4bb76d7d9a06e0bfb472f3e0
Oliver Gaskell <olivergaskell700@gmail.com>
2022-01-31 19:47:15 +0000

    Remove __hash__ and generate_id from Node().
    As Nodes will not be directly added to the graph structure, the hash 
    function is no longer required. This means the Node's UID's only 
    function is to identify it in the real world, as such it makes no sense 
    to generate this randomly so generate_id was removed.

--- autonopi/navigation.py
+++ autonopi/navigation.py
@@ -24,28 +24,7 @@ class Navigation:
 class Node:
     """A Single node within a navigation graph."""
 
-    def __init__(self, nav: Navigation, id: int = None):
+    def __init__(self, nav: Navigation, id: int):
         self.parent = nav  # Store reference to parent class
 
-        if id is None:  # Check if an ID is provided.
-            self.id = self.generate_id()  # If not, generate one.
-        else:
-            self.id = id
-
-    def __hash__(self) -> int:
-        """Return the ID of the Node as a hash value.
-
-        Technically this is a very incorrect was of calculating a hash, since
-        it should be calculated based on the objects data. However, the Node
-        doesn't really have any data the hash may be calculated from, and the ID
-        will be unique so should suffice for my use.
-        """
-        return self.id
-
-    def generate_id(self) -> int:
-        """Generate a unique ID for the current node.
-
-        Currently just a template, will need to be replaced with functional
-        code at a later date.
-        """
-        return 0
+        self.id = id
