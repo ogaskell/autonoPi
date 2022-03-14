5a52cb14c987a7011920529cdad9c70b7f37ab13
Oliver Gaskell <olivergaskell700@gmail.com>
2022-03-14 22:29:59 +0000

    Fix type annotation for fetch_image.
    The original annotation was using the class name C++'s OpenCV uses, not 
    Python's.

--- autonopi/cv.py
+++ autonopi/cv.py
@@ -2,6 +2,7 @@
 
 """Computer Vision module."""
 import cv2
+import numpy
 
 camera = cv2.VideoCapture(0)
 
@@ -12,7 +13,7 @@ class LineDetector:
     def __init__(self, cam: cv2.VideoCapture):
         self.cam = cam  # Store reference to the VideoCapture object
 
-    def fetch_image(self, flag: int = 1) -> cv2.OutputArray:
+    def fetch_image(self, flag: int = 1) -> numpy.ndarray:
         """Read an image from camera."""
         ret, frame = self.cam.read()
         if ret:
