1eb907c371a626274771ef5a38bb06ef24aa0f9f
Oliver Gaskell <olivergaskell700@gmail.com>
2022-03-14 14:34:54 +0000

    Add basic LineDetector class. This class will include code for the Canny Line Detector that will be used to detect lanes.

--- autonopi/cv.py
+++ autonopi/cv.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
 """Computer Vision module."""
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+
+class LineDetector:
+    """Canny Line Detector."""
+
+    def __init__(self, cam: cv2.VideoCapture):
+        self.cam = cam  # Store reference to the VideoCapture object
