2d4048a0dbf9233da5e06c0220d35f87313ff9cc
Oliver Gaskell <olivergaskell700@gmail.com>
2022-03-23 20:45:32 +0000

    Create simple management system template.
    This template inclues the main event loop and component setup, which is 
    the base minimum for a funtioning implementation of autonoPi.

--- autonopi/management.py
+++ autonopi/management.py
@@ -1,3 +1,42 @@
 #!/usr/bin/env python3
 
 """Management system module."""
+
+
+from .cv import LineDetector
+from .hardware.motion import Motion
+from .navigation import Navigation
+
+
+class Manager:
+    """Management system base class. This will also include base classes for any components.
+
+    In order to implement autonoPi, create a class which inherits from this class.
+    """
+
+    def __init__(self):
+        self.setup_components()
+
+    def setup_components(self) -> None:
+        """Setup components required by the system.
+
+        This only uses the base classes for most systems so should be overwritten in an implmentation.
+        """
+        self.line_detector = LineDetector()
+        self.motion = Motion()
+        self.navigation = Navigation()
+
+    def mainloop(self) -> None:
+        """Main event loop of the program.
+
+        Will simply run self.run continuously.
+        """
+        while True:
+            self.run()
+
+    def run(self) -> None:
+        """The main event loop code goes here.
+
+        Note that this function should run and return, since the loop is done elsewhere.
+        """
+        raise NotImplementedError("Manager() is the base management class, and subclasses must overwrite run().")
